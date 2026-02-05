6771b3900ad0313548ca7ab7692fdda7e9325503
Karin Zohar <karinzohar3@gmail.com>
2026-02-05 21:28:57 +0200

    feat: add custom favicon

--- src/components/MovieDisplay/MovieDisplay.tsx
+++ src/components/MovieDisplay/MovieDisplay.tsx
@@ -1,4 +1,4 @@
-import type {FC} from "react";
+import type { FC } from "react";
 import type { Movie } from "./MovieDisplay.type";
 import MovieList from "./components/MovieList/MovieList";
 
@@ -6,7 +6,8 @@ type MovieDisplayProps = {
   movies: Movie[];
 };
 
-const MovieDisplay: FC<MovieDisplayProps> = ({ movies  }) => {
+//TODO: handle async & pagination here 
+const MovieDisplay: FC<MovieDisplayProps> = ({ movies }) => {
   return (
     <div className="movie-display">
       <MovieList movies={movies} />
