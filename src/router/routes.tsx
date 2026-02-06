f5a48b7b31b9c97e8f2371ded1bb0f15347ef236
Karin Zohar <karinzohar3@gmail.com>
2026-02-06 18:26:02 +0200

    perf: remove lazy load from routes

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,8 +1,8 @@
-import { lazy } from "react";
 import { type RouteObject } from "react-router";
 
-const HomePage = lazy(async () => await import("@/pages/HomePage/HomePage"));
-const MoviePage = lazy(async () => await import("@/pages/MoviePage/MoviePage"));
+import HomePage from '@/pages/HomePage/HomePage';
+import MoviePage from '@/pages/MoviePage/MoviePage';
+
 const routes: RouteObject[] = [
   {
     path: "/",
