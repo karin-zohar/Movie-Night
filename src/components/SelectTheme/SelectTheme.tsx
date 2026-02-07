c7365908fa1f3963ca3745447c922438186dc2cd
Karin Zohar <karinzohar3@gmail.com>
2026-02-07 18:26:36 +0200

    fix: adjust styling to match themes

--- src/components/SelectTheme/SelectTheme.tsx
+++ src/components/SelectTheme/SelectTheme.tsx
@@ -3,20 +3,21 @@ import { MoonIcon, SunIcon } from "@/libs/ui/icons";
 import { useTheme } from "@/store";
 import GenSelect from "@/libs/ui/components/GenSelect/GenSelect";
 
+const themeOptions = [
+    {
+        label: <span><SunIcon /> <span> Light Mode</span></span>,
+        value: "light",
+        title: 'Light mode'
+    },
+    {
+        label: <span><MoonIcon /> <span> Dark Mode</span></span>,
+        value: "dark",
+        title: 'Dark Mode'
+    },
+];
+
 const SelectTheme = () => {
     const { theme, setTheme } = useTheme();
-    const themeOptions = [
-        {
-            label: <SunIcon />,
-            value: "light",
-            title: 'Light mode'
-        },
-        {
-            label: <MoonIcon />,
-            value: "dark",
-            title: 'Dark Mode'
-        },
-    ];
 
 
     return (
